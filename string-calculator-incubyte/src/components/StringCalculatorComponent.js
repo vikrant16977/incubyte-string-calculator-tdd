cfafa18596ed6575e35b0f04ef83c5d383a1fee5
Vikrant Aher <vikrant@Vikrants-MacBook-Air.local>
2025-01-31 19:10:12 +0530

    Updated to display loading when button is clicked and show output with a delay

--- string-calculator-incubyte/src/components/StringCalculatorComponent.js
+++ string-calculator-incubyte/src/components/StringCalculatorComponent.js
@@ -1,4 +1,4 @@
-import React,{ useState} from "react";
+import React,{ useState ,useEffect} from "react";
 import { add } from "../utils/stringCalculatorLogic";
 import "semantic-ui-css/semantic.min.css";
 import { Button, TextArea, Container, Header, Message, Icon, Popup,Card } from "semantic-ui-react";
@@ -8,18 +8,30 @@ function StringCalculatorComponent() {
     const [stringInput,setStringInput]=useState("")
     const [result,setResult]=useState("")
     const [error, setError] = useState(null);
+    const [loading, setLoading]= useState(false)
 
+
+
+    useEffect(()=>{
+        if(loading){
+              const output = add(stringInput);
+              setTimeout(()=>{setResult(`Result: ${output}`); setLoading(false);}, 3000);
+             
+        }
+        
+    },[loading])
   const  handleSum=()=>{
         try {
             setError(null);
-            const output = add(stringInput);
-            setResult(`Result: ${output}`);
+            setLoading(true);
         } catch (error) {
             setResult("");
             setError(error.message);
+             setLoading(false);
         }
     }
     const renderRulesCard = () => (
+        
         <Card style={{ marginLeft:"left", marginTop: "20px", backgroundColor: "#f9f9f9", padding: "20px" ,width:'100%'}}>
             <Card.Content>
                 <Card.Header style={{ color: "#2185d0", fontSize: "1.5em" }}>Rules for Valid Input</Card.Header>
@@ -43,6 +55,7 @@ function StringCalculatorComponent() {
     );
 
     return (
+        
         <div style={{
             background: "linear-gradient(135deg, #6a11cb 0%, #2575fc 100%)",
             minHeight: "100vh",
@@ -51,6 +64,7 @@ function StringCalculatorComponent() {
             justifyContent: "center",
             padding: "20px",
         }}>
+         { 
         <Container textAlign="center" style={{ background: "#fff", borderRadius: "10px", padding: "30px", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)" }}>
             <Header as="h1" style={{ color: "#2185d0", fontSize: "3em", marginBottom: "20px" }}>String Calculator</Header>
             <Popup
@@ -66,7 +80,7 @@ function StringCalculatorComponent() {
             content="You can use custom delimiters by starting with // followed by the delimiter and \n. For example: //;\n1;2"
                 position="top center"/>
             <br />
-            <Button
+         { loading?<Button  disabled={true}>Loading..</Button> : <Button
                 primary
                 onClick={handleSum}
                 style={{
@@ -77,6 +91,7 @@ function StringCalculatorComponent() {
             >
                  <Icon name="calculator" /> Calculate
             </Button>
+}
             {result && (
                 <Message positive style={{  marginTop: "20px", fontSize: "1.5em"  }}>
                      <Icon name="check circle" /> {result}
@@ -89,7 +104,9 @@ function StringCalculatorComponent() {
             )}
              {renderRulesCard()}
         </Container>
+}
         </div>
+        
     );
 
 }
